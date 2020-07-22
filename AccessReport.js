40ba8791277a4ad3855b40535f9a145ce937fbb9
Anjan Kumar <kumar.rakuditi@gmail.com>
2020-07-22 11:01:59 +0530

    initial commit

---
+++ AccessReport.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const BASE_URI = 'https://api.github.com';
+const API_KEY = process.env.API_KEY;
+const ORG_CODE = 'realvariabledas';
+const API = {
+    MEMBER_LIST: BASE_URI + '/orgs/' + ORG_CODE + '/members',
+    TEAMS_LIST: BASE_URI + '/orgs/' + ORG_CODE + '/teams'
+};
+let USERS = [];
+let TEAMS = [];
+let requiredData = [];
+function doCall(url) {
+    let noRecordsFound = false;
+    let page = 1;
+    return new Promise(async (resolve, reject) => {
+        let fullData = [];
+        while(!noRecordsFound) {
+            const data = await fetch(url + '?page=' + page, { headers: { 'Authorization': 'bearer ' + API_KEY } });
+            page++;
+            const pageData = await data.json();
+            if (pageData.length === 0) {
+                noRecordsFound = true;
+            } else {
+                // console.log('page', pageData);
+                fullData.push(...pageData);
+            }
+        }
+        resolve(fullData);
+    });
+
+}
+
+async function fetchData() {
+    USERS = await doCall(API.MEMBER_LIST);
+    TEAMS = await doCall(API.TEAMS_LIST);
+    const aloop = async() => {
+        for (let i = 0; i < TEAMS.length; i++) {
+            let element = TEAMS[i];
+            console.log(element);
+            element.members_url = element.members_url.replace('{/member}', '');
+            if (element.members === undefined) {
+                element.members = [];
+            }
+            if (element.repos === undefined) {
+                element.repos = [];
+            }
+            element.members.push(...await doCall(element.members_url));
+            element.repos.push(...await doCall(element.repositories_url));
+            TEAMS[i] = element;
+        }
+    }
+    await aloop();
+    TEAMS.forEach(team => {
+        team.members.forEach(member => {
+            const userIndex = USERS.findIndex(user => user.login === member.login);
+            if (USERS[userIndex].repos === undefined) {
+                USERS[userIndex].repos = [];
+            }
+            USERS[userIndex].repos.push(...team.repos);
+        });
+    });
+    USERS.forEach(user => {
+        if (user.repos === undefined) {
+            user.repos = [];
+        }
+        console.log('User', user.login, user.repos.length);
+        user.repos.forEach(repo => {
+            requiredData.push({'user': user.login, repo});
+        });
+    })
+    console.log('================    Report Generation Started     ==========================');
+    let data = 'User,Repo Name,Pull,Triage,Push,Maintain,Admin\n'; 
+    requiredData.map(v => data += v.user + ', ' + v.repo.name + ', ' + v.repo.permissions.pull + ', ' + v.repo.permissions.triage + ', ' + v.repo.permissions.push + ', ' + v.repo.permissions.maintain + ', ' + v.repo.permissions.admin + '\n');
+    const date = new Date();
+    const timestamp = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '_' + date.getHours() + '-' + date.getMinutes() + '-' + date.getSeconds();
+    const fileName = 'Report_' + timestamp + '.csv';
+    fs.writeFileSync(fileName, data);
+    console.log('File name: ' + fileName);
+    console.log('================    Report Generation Completed   ==========================');
+
+}
+fetchData();
